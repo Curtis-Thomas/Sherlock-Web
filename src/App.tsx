ec2d5b7aa33f2028b39edc8312eeeaec0d38d1eb
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2024-06-04 11:38:17 +0300

    add: storage section, assimilated sessions ection

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import BrowserCapabilities from "./UserInfo/BrowserCapabilities";
 // import Referrer from "./UserInfo/Referrer";
 import Locale from "./UserInfo/Locale";
 import Hardware from "./UserInfo/Hardware";
-import Session from "./UserInfo/Session";
+import Storage from "./UserInfo/Storage";
 
 function App() {
   return (
@@ -39,7 +39,7 @@ function App() {
         </Typography>
       </Box>
       <Box sx={{ pb: 5 }}>
-        <Box>
+        <Box sx={{ pt: 1, pb: 1 }}>
           <Typography variant="h5" sx={{ textAlign: "center" }}>
             A simple web application that provides information about the user's
             device, operating system, network, browser capabilities, locale,
@@ -55,23 +55,34 @@ function App() {
           <Grid item xs={12} sm={6} sx={{ border: "dashed 1px #75A4FF", p: 1 }}>
             <OperatingSystem />
           </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            sx={{ border: "dashed 1px #75A4FF", p: 1 }}
+          >
+            <BrowserCapabilities />
+          </Grid>
           <Grid item xs={12} sm={6} sx={{ border: "dashed 1px #75A4FF", p: 1 }}>
             <Network />
           </Grid>
-          <Grid item xs={12} sm={6} sx={{ border: "dashed 1px #75A4FF", p: 1 }}>
-            <BrowserCapabilities />
-          </Grid>
+
           {/* <Grid item xs={12} sm={6} >
             <Referrer />
           </Grid> */}
           <Grid item xs={12} sm={6} sx={{ border: "dashed 1px #75A4FF", p: 1 }}>
             <Locale />
           </Grid>
-          <Grid item xs={12} sm={6} sx={{ border: "dashed 1px #75A4FF", p: 1 }}>
-            <Hardware />
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            sx={{ border: "dashed 1px #75A4FF", p: 1 }}
+          >
+            <Storage />
           </Grid>
           <Grid item xs={12} sm={6} sx={{ border: "dashed 1px #75A4FF", p: 1 }}>
-            <Session />
+            <Hardware />
           </Grid>
         </Grid>
       </Box>
---
+++ src/UserInfo/Storage.tsx
@@ -0,0 +1,90 @@
+import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+
+function Storage() {
+  const [cookies, setCookies] = useState<Record<string, string>>({});
+  const [localStorageData, setLocalStorageData] = useState<Record<string, any>>(
+    {}
+  );
+  const [sessionStorageData, setSessionStorageData] = useState<
+    Record<string, any>
+  >({});
+
+  useEffect(() => {
+    const cookieString = document.cookie;
+    const cookieArray = cookieString.split("; ");
+    const cookieObject = cookieArray.reduce((acc, cookie) => {
+      const [key, value] = cookie.split("=");
+      return { ...acc, [key]: value };
+    }, {});
+    setCookies(cookieObject);
+
+    const parseValue = (value: string) => {
+      try {
+        return JSON.parse(value);
+      } catch {
+        return value;
+      }
+    };
+
+    const localStorageObject = Object.entries(localStorage).reduce(
+      (acc, [key, value]) => {
+        return { ...acc, [key]: parseValue(value) };
+      },
+      {}
+    );
+    setLocalStorageData(localStorageObject);
+
+    const sessionStorageObject = Object.entries(sessionStorage).reduce(
+      (acc, [key, value]) => {
+        return { ...acc, [key]: parseValue(value) };
+      },
+      {}
+    );
+    setSessionStorageData(sessionStorageObject);
+  }, []);
+
+  const getType = (value: any) => {
+    if (typeof value === "object" && value !== null) {
+      if (value instanceof Date) {
+        return "date";
+      } else {
+        return "object";
+      }
+    }
+    return typeof value;
+  };
+
+  return (
+    <>
+      <Typography
+        textAlign={"center"}
+        sx={{ pb: 2 }}
+        variant="h5"
+        color={"#75A4FF"}
+      >
+        Storage
+      </Typography>
+      <Typography sx={{ pb: 1 }}>Cookies:</Typography>
+      {Object.entries(cookies).map(([key, value]) => (
+        <Typography key={key} sx={{ pb: 1 }}>
+          {key} ({getType(value)}): {String(value)}
+        </Typography>
+      ))}
+      <Typography sx={{ pb: 1, pt: 1 }}>Local Storage:</Typography>
+      {Object.entries(localStorageData).map(([key, value]) => (
+        <Typography key={key} sx={{ pb: 1 }}>
+          {key} ({getType(value)}): {JSON.stringify(value, null, 2)}
+        </Typography>
+      ))}
+      <Typography sx={{ pb: 1, pt: 1 }}>Session Storage:</Typography>
+      {Object.entries(sessionStorageData).map(([key, value]) => (
+        <Typography key={key} sx={{ pb: 1 }}>
+          {key} ({getType(value)}): {JSON.stringify(value, null, 2)}
+        </Typography>
+      ))}
+    </>
+  );
+}
+
+export default Storage;
